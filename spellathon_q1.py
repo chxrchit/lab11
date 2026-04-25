e597410f0027feb54f9198d3268c68d5d1d0ec13
chxrchit <agarwalcharchit2007@gmail.com>
2026-04-25 18:05:49 +0530

    Refactor Spellathon class and fix attempt tracking

--- spellathon_q1.py
+++ spellathon_q1.py
@@ -1,16 +1,16 @@
-class Spellathon
+class Spellathon:
     def __init__(self, data):
+        self.letters = data[0]
         self.data = data[1:]
         self.score = 0
-        self.attempt = 0
-        self.letters = data[0]
+        self.attempts = 0
         self.remaining_words = sorted(self.data)
-    
+
     def play(self, word):
-        self.attempt += 1
+        self.attempts += 1
         if len(word) >= 4 and word in self.data:
             self.score += 1
             self.data.remove(word)
-            return ('Correct guess!')
-        else:
-            return('Incorrect guess!')
+            self.remaining_words = sorted(self.data)
+            return 'Correct guess!'
+        return 'Incorrect guess!'
